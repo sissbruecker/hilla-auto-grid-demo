21a5f8ba3d7267fe3348d4ef0642581df9f433a9
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-09-20 22:13:55 +0200

    Bump Hilla version

--- src/main/java/com/example/application/ProductService.java
+++ src/main/java/com/example/application/ProductService.java
@@ -2,14 +2,8 @@ package com.example.application;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.BrowserCallable;
-import dev.hilla.Nonnull;
-import dev.hilla.Nullable;
 import dev.hilla.crud.CrudRepositoryService;
-import dev.hilla.crud.filter.Filter;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @BrowserCallable
 @Service
@@ -17,11 +11,5 @@ import java.util.List;
 public class ProductService extends CrudRepositoryService<Product, Long> {
     public ProductService(ProductRepository productRepository) {
         super(Product.class, productRepository);
-    }
-
-    // Workaround for exposing superclass-method
-    @Override
-    public @Nonnull List<@Nonnull Product> list(Pageable pageable, @Nullable Filter filter) {
-        return super.list(pageable, filter);
     }
 }
